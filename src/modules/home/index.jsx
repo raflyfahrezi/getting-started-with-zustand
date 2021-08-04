c5268f754f28b50bdc976341b500a67a4f0962ef
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-05 06:00:22 +0700

    adding button in home

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
 import React from 'react'
+import Link from 'next/link'
 
 import useStore from '../../zustand'
 
@@ -14,18 +15,25 @@ const home = () => {
     return (
         <div className='w-full text-center mt-10'>
             <p className='text-3xl mb-7'>{state.count}</p>
-            <button
-                onClick={state.increment}
-                className='p-3 bg-green-300 rounded-md mr-5'
-            >
-                Increment
-            </button>
-            <button
-                onClick={state.decrement}
-                className='p-3 bg-red-300 rounded-md'
-            >
-                Decrement
-            </button>
+            <div>
+                <button
+                    onClick={state.increment}
+                    className='p-3 bg-green-300 rounded-md mr-5'
+                >
+                    Increment
+                </button>
+                <button
+                    onClick={state.decrement}
+                    className='p-3 bg-red-300 rounded-md'
+                >
+                    Decrement
+                </button>
+            </div>
+            <div className='mt-5'>
+                <Link href='/more' passHref>
+                    <a>Click here for more</a>
+                </Link>
+            </div>
         </div>
     )
 }
