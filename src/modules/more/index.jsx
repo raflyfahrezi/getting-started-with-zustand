991dc152a0e8d008d285acbfd752863466a922ec
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-05 05:57:19 +0700

    implement zustand on more page

--- src/modules/more/index.jsx
+++ src/modules/more/index.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import React from 'react'
 
+import useStore from '../../zustand'
+
 const more = () => {
+    const state = {
+        count: useStore((state) => state.count),
+    }
+
     return (
-        <div>
-            <div></div>
+        <div className='w-full mt-10 text-center'>
+            <p className='text-3xl mb-7'>You have {state.count} count</p>
         </div>
     )
 }
