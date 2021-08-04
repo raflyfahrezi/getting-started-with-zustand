3834ce112f21fe763dc09911a748fa408bae50a1
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-05 05:47:04 +0700

    layouting home modules

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const home = () => {
     return (
-        <div>
-            <div></div>
+        <div className='w-full text-center mt-10'>
+            <p className='text-3xl mb-7'>0</p>
+            <button className='p-3 bg-green-300 rounded-md mr-5'>
+                Increment
+            </button>
+            <button className='p-3 bg-red-300 rounded-md'>Decrement</button>
         </div>
     )
 }
