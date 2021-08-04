18516a9ac9ba3d40f56f899912354dbbd1b5d5ed
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-08-05 05:50:56 +0700

    implement zustand

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -1,13 +1,31 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import React from 'react'
 
+import useStore from '../../zustand'
+
 const home = () => {
+    const state = {
+        count: useStore((state) => state.count),
+        increment: useStore((state) => state.increment),
+        decrement: useStore((state) => state.decrement),
+    }
+
     return (
         <div className='w-full text-center mt-10'>
-            <p className='text-3xl mb-7'>0</p>
-            <button className='p-3 bg-green-300 rounded-md mr-5'>
+            <p className='text-3xl mb-7'>{state.count}</p>
+            <button
+                onClick={state.increment}
+                className='p-3 bg-green-300 rounded-md mr-5'
+            >
                 Increment
             </button>
-            <button className='p-3 bg-red-300 rounded-md'>Decrement</button>
+            <button
+                onClick={state.decrement}
+                className='p-3 bg-red-300 rounded-md'
+            >
+                Decrement
+            </button>
         </div>
     )
 }
